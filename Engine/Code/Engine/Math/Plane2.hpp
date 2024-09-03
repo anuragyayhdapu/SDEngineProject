811d68a1f20785229d1419a7a3030cd79b455420
Anurag (Anku) Upadhyay <anuragyayhdapu@gmail.com>
2024-09-02 21:00:25 -0500

    default initialized class members

--- Engine/Code/Engine/Math/Plane2.hpp
+++ Engine/Code/Engine/Math/Plane2.hpp
@@ -4,6 +4,6 @@
 //----------------------------------------------------------------------------------------------------------
 struct Plane2
 {
-	Vec2  m_normal;
-	float m_distanceFromOrigin;
+	Vec2  m_normal			   = Vec2::ZERO;
+	float m_distanceFromOrigin = 0.f;
 };
--- Engine/Code/Engine/Math/Plane3.hpp
+++ Engine/Code/Engine/Math/Plane3.hpp
@@ -10,8 +10,8 @@ struct Plane3
 	explicit Plane3();
 	explicit Plane3(Vec3 const& normal, float distanceFromOrigin);
 
-	Vec3  m_normal;
-	float m_distanceFromOrigin;
+	Vec3  m_normal			   = Vec3::ZERO;
+	float m_distanceFromOrigin = 0.f;
 
 	static const Plane3 MakeFromVectorsInPlane(Vec3 const& A, Vec3 const& B);
 };
